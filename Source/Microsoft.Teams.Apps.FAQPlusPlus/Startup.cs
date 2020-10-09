42e8bf9e20031e048776b90f26f014fe3b1ee461
Abdelrady Tantawy <v-abtan@microsoft.com>
2020-10-09 15:34:39 -0700

    Test different auths for different bots

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Startup.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Startup.cs
@@ -128,25 +128,25 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus
             services.AddSingleton<IMemoryCache, MemoryCache>();
             services.AddTransient(sp => (BotFrameworkAdapter)sp.GetRequiredService<IBotFrameworkHttpAdapter>());
 
-            services.AddSingleton(new MicrosoftAppCredentials(this.Configuration[UserBotMicrosoftAppId], this.Configuration[UserBotMicrosoftAppPassword]));
+            //services.AddSingleton(new MicrosoftAppCredentials(this.Configuration[UserBotMicrosoftAppId], this.Configuration[UserBotMicrosoftAppPassword]));
             //services.AddSingleton(new MicrosoftAppCredentials(this.Configuration[SmeBotMicrosoftAppId], this.Configuration[SmeBotMicrosoftAppPassword]));
 
-            services.AddTransient<SmeActivityHandler>();
-            services.AddTransient<UserActivityHandler>();
+            //services.AddTransient<SmeActivityHandler>();
+            //services.AddTransient<UserActivityHandler>();
 
-            //services.AddTransient(provider =>
-            //    ActivatorUtilities.CreateInstance<SmeActivityHandler>(
-            //        provider,
-            //        new MicrosoftAppCredentials(
-            //            this.Configuration[SmeBotMicrosoftAppId],
-            //            this.Configuration[SmeBotMicrosoftAppPassword])));
+            services.AddTransient(provider =>
+                ActivatorUtilities.CreateInstance<SmeActivityHandler>(
+                    provider,
+                    new MicrosoftAppCredentials(
+                        this.Configuration[SmeBotMicrosoftAppId],
+                        this.Configuration[SmeBotMicrosoftAppPassword])));
 
-            //services.AddTransient(provider =>
-            //    ActivatorUtilities.CreateInstance<UserActivityHandler>(
-            //        provider,
-            //        new MicrosoftAppCredentials(
-            //            this.Configuration[UserBotMicrosoftAppId],
-            //            this.Configuration[UserBotMicrosoftAppPassword])));
+            services.AddTransient(provider =>
+                ActivatorUtilities.CreateInstance<UserActivityHandler>(
+                    provider,
+                    new MicrosoftAppCredentials(
+                        this.Configuration[UserBotMicrosoftAppId],
+                        this.Configuration[UserBotMicrosoftAppPassword])));
 
             // Create the telemetry middleware(used by the telemetry initializer) to track conversation events
             services.AddSingleton<TelemetryLoggerMiddleware>();
